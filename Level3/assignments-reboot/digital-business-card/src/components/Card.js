2b6a9b3dfdacbc98b75c156f812c4b7b773edd1c
Dave Roberts <56813289+lohidenver@users.noreply.github.com>
2023-04-08 19:20:16 -0600

    Add files via upload

---
+++ Level3/assignments-reboot/digital-business-card/src/components/Card.js
@@ -0,0 +1,17 @@
+//
+import ProfilePicture from "./ProfilePicture";
+import Main from "./Main";
+import Footer from "./Footer"
+
+function Card() {
+    return (
+      <div className="Card">
+        <ProfilePicture/>
+        <Main/>
+        <Footer/>
+      </div>
+    );
+  }
+  
+  export default Card;
+  
---
+++ Level3/assignments-reboot/digital-business-card/src/components/Main.js
@@ -0,0 +1,28 @@
+function Main() {
+  return (
+    <div className='Main'>
+      <div className='top-main'>
+        <h1>Dave Roberts</h1>
+        <h3>Front End Developer</h3>
+        <h6>glitchinthematri.x</h6>
+        <div className='links'>
+          <button id='email'><a href="">E-Mail</a> </button>
+          <button id='linked-in'>
+            <a href="">
+
+            LinkedIn
+            </a>
+            </button>
+        </div>
+      </div>
+      <div className="bottom-main">
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae placeat recusandae vero ab dicta perspiciatis corrupti vel excepturi mollitia magni? Aut vitae praesentium deserunt magni quia rem dolorem quasi sunt.</p>
+        <h2>Interests</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt tempore incidunt placeat odio explicabo sed ea? Tempora neque odio dolores. Itaque excepturi dolor soluta sequi explicabo optio molestiae fugiat dolorum!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
