10ed29f007e58dd23bc2a6a6fa0149e89eed7b81
Dave Roberts <56813289+lohidenver@users.noreply.github.com>
2023-04-30 00:15:00 -0600

    Add files via upload
    A sample plumbing website that demonstrates react router and changing dark/light mode

---
+++ Level4/react-router-plumbing-demo/react-roto-router/src/components/Careers.js
@@ -0,0 +1,233 @@
+import React from "react";
+
+const Careers = () => {
+  return (
+    <>
+      <div className='careers'>
+        <h2>Careers</h2>
+        <h3>Plumbing Technician</h3>
+        <h3>Job details</h3>
+        <h4>Job Type</h4>
+        <h4>Full-time</h4>
+        <p>
+          We are currently seeking a plumbing technician at the journeyman and
+          master level for our Dennysville Energy Services branch. The position
+          includes planning and performing preventive maintenance and emergency
+          repairs on plumbing systems, as well as maintaining the inspection
+          backflow preventer and boilers database.
+        </p>
+        <br />
+        <br />
+        <p>
+          Ask us about our $3000 sign on bonus! This opportunity includes
+          competitive and comprehensive benefits
+        </p>
+        <br />
+        <br />
+        <p>401 (K) plan, with company match</p>
+        <br />
+        <br />
+        <p>Health Insurance, including family coverage options</p>
+        <br />
+        <br />
+        <p>Dental Insurance</p>
+        <br />
+        <br />
+        Vision Insurance
+        <br />
+        <br />
+        Life Insurance
+        <br />
+        <br />
+        Short Term and Long Term Disability
+        <br />
+        <br />
+        Paid Time Off, including six paid holidays
+        <br />
+        <br />
+        Continuing education reimbursements
+        <br />
+        <br />
+        Discounts on Company products and services
+        <br />
+        <br />
+        Competitive salary commensurate with experience Essential Duties and
+        Responsibilities include the following. Other duties may be assigned.
+        <br />
+        <br />
+        Maintains and repairs general plumbing components to include toilets,
+        urinals, faucets, hose bibs, and drains.
+        <br />
+        <br />
+        Installs, repairs, or replaces domestic hot and cold-water lines.
+        <br />
+        <br />
+        Repairs and replaces steam to hot water exchangers for major building
+        heating systems and pools.
+        <br />
+        <br />
+        Repairs and replaces pneumatic steam valves, radiator valves, traps, and
+        steam and condensate pipes.
+        <br />
+        <br />
+        Repairs and replaces domestic hot water heat pumps, chilled water pumps,
+        sump pumps, and sewage pumps.
+        <br />
+        <br />
+        Installs, tests, and repairs all sizes of backflow prevention devices.
+        <br />
+        <br />
+        Ensures proper care in the use and maintenance of equipment and
+        supplies; promotes continuous improvement of workplace safety and
+        environmental practices.
+        <br />
+        <br />
+        Provides sketches and dimensions used by drafters to create plans for
+        subcontractor work.
+        <br />
+        <br />
+        Research catalogs and vendor services for complex and/or customized
+        system retrofits.
+        <br />
+        <br />
+        Interprets and applies mechanical and structural blueprints to plan
+        plumbing repairs and modifications.
+        <br />
+        <br />
+        In coordination with management, assigns to and tracks corrective and
+        preventive maintenance tasks given to subordinate plumbers.
+        <br />
+        <br />
+        Performs miscellaneous job-related duties as assigned.
+        <br />
+        <br />
+        Completes Accurate Work Orders with Follow Up Needs along with all
+        Parts/Parts Numbers Used and Needed
+        <br />
+        <br />
+        Effectively Communicates Service Performed, and when necessary, Follow
+        Up Needed to the Customer
+        <br />
+        <br />
+        Supports Mario’s Growth Initiative by Recommending Products and Services
+        to Increase the Customer’s System Performance and Efficiency
+        <br />
+        <br />
+        Mario’s a “Whole Team” approach when Communicating and Working with
+        Dispatch, Office, Delivery and Fellow Service Technicians
+        <br />
+        <br />
+        Maintains Personal Appearance in Accordance with Mario’s Standards of
+        Excellence and wears the appropriate company uniform.
+        <br />
+        <br />
+        Maintains Company Issued Vehicle in Accordance with Mario’s Standards of
+        Excellence
+        <br />
+        <br />
+        <h3>EDUCATION and/or EXPERIENCE</h3>
+        <br />
+        <br />
+        High school diploma or general education degree (GED); or related
+        experience and/or training; or equivalent combination of education and
+        experience. Possession of a current New York Master/Journeyman
+        Plumber/Gas Fitter (JPG) License. ç
+        <br />
+        <br />
+        <h3>CERTIFICATES, LICENSES, REGISTRATIONS</h3>
+        <br />
+        <br />
+        <p>
+          Must possess a State of New York driver’s license with a clean driving
+          record.
+        </p>
+        <br />
+        <br />
+        <p>Must be certified by the Company in each of the following areas:</p>
+        <br />
+        <br />
+        <p>
+          Possession of a valid State driver's license is a requirement for this
+          job.
+        </p>
+        <br />
+        <br />
+        <p>Must pass a pre-employment criminal background check.</p>
+        <br />
+        <br />
+        <p>
+          Successful candidate must submit to post-offer, pre-employment
+          physical examination.
+        </p>
+        <br />
+        <br />
+        <p>New York Master/Journeyman Plumber/Gas Fitter (JPG) License</p>
+        <br />
+        <br />
+        <p>Must be willing to obtain CDL Hazmat Endorsement</p>
+        <br />
+        <br />
+        <h3>PHYSICAL DEMANDS</h3>
+        <br />
+        <br />
+        <ul>
+          <li>
+            While performing the duties of this Job, the employee is regularly
+            required to stand; walk; use hands to finger, handle, or feel; climb
+            or balance; stoop, kneel, crouch, or crawl; talk or hear and taste
+            or smell.
+          </li>
+          <li>
+            The employee is frequently required to reach with hands and arms.
+          </li>
+          <li>The employee is occasionally required to sit.</li>
+          <li>
+            The employee must regularly lift and/or move up to 100 pounds,
+            frequently lift and/or move up to 50 pounds and occasionally lift
+            and/or move more than 100 pounds.
+          </li>
+          <li>
+            Specific vision abilities required by this job include close vision,
+            distance vision, color vision, peripheral vision, depth perception
+            and ability to adjust focus.
+          </li>
+        </ul>
+        <br />
+        <br />
+        <h3>WORK ENVIRONMENT</h3>
+        <br />
+        <br />
+        <p>
+
+          While performing the duties of this Job, the employee is regularly
+          exposed to moving mechanical parts; fumes or airborne particles; toxic
+          or caustic chemicals and risk of electrical shock. The employee is
+          frequently exposed to wet and/or humid conditions. The employee is
+          occasionally exposed to high, precarious places; outside weather
+          conditions; extreme cold; extreme heat and explosives. The noise level
+          in the work environment is usually loud.
+        
+        </p>
+          <br />
+          <br />
+          <p>
+          NOTE: These statements are intended to describe the general nature and
+          level of work involved for this job. It is not an exhaustive list of
+          all responsibilities, duties and skills required of this job.
+          </p>
+          <br />
+          <p>
+            Mario is an Equal Opportunity/Affirmative Action employer. All
+            qualified applicants are considered for all positions without regard
+            to race, color, religion, sex, ancestry or national origin, age,
+            sexual orientation, marital status, disability, protected veteran
+            status or any other legally protected status.
+          </p>
+          <br />
+        
+      </div>
+    </>
+  );
+};
+
+export default Careers;
---
+++ Level4/react-router-plumbing-demo/react-roto-router/src/components/Services.js
@@ -0,0 +1,173 @@
+import React from "react";
+
+const Services = () =>{
+    return (
+        <div className="services">
+            <h1>
+
+            Plumbing Services in New York City
+            </h1>
+            <h2>
+
+Providing a Better Way to Fix Your Plumbing
+            </h2>
+            <p>
+
+If you require plumbing services in New York City for your home or business, Mario Services, Inc. is here to help. With over 40 years of experience, we have everything necessary to get the job done right every time. Whether you have a leaky faucet, clogged drain, broken water heater, busted pipe, or any other plumbing issue, we will provide a fast and effective solution.
+            </p>
+<h2>
+
+To get your plumbing system in tip-top shape, call (718) 557-9604 now and schedule your immediate service!
+</h2>
+
+<h2>
+
+Our NYC Plumbing Services
+</h2>
+<p>
+
+At Mario Services, Inc., we handle all plumbing-related issues. From simple repairs and routine maintenance to the installation of a brand-new plumbing system, our licensed plumbers are trained to do it all.
+</p>
+
+<h2>
+
+We offer a wide range of plumbing services, including:
+</h2>
+<br />
+<ul>
+<li>
+Drain cleaning and repair
+    
+</li>
+<li>
+Plumbing fixtures
+</li>
+<li>
+Leak detection
+</li>
+<li>
+Water heater repair
+</li>
+<li>
+Water heater replacement
+</li>
+<li>
+Gas lines
+</li>
+<li>
+
+Heat pumps
+</li>
+<li>
+Boiler repair and installation
+</li>
+<li>
+Backflow prevention and testing
+    
+</li>
+<li>
+Plumbing code violation removal
+</li>
+<li>
+Piping and repiping
+</li>
+<li>
+New construction
+</li>
+<li>
+Commercial plumbing
+</li>
+<li>
+Emergency plumbing
+</li>
+
+
+
+
+
+</ul>
+<br />
+<h2>
+
+We’re Driven to Ensure Your Satisfaction
+</h2>
+<p>
+
+If you are like most people, you cringe at the thought of a plumber stepping into your property. Plumbers handle dirty tasks and often leave a trail of filth as they traipse through houses and businesses. Unfortunately, this is an accurate expectation of the kind of service many plumbers deliver. But you deserve better!
+</p>
+
+<br />
+<p>
+
+At Mario Services, Inc., we take a much different approach. To deliver the best possible customer experience, we do not just provide high-quality products and exceptional workmanship. We also go the extra mile to treat you and your home with respect. What does that look like practically?
+</p>
+<br />
+<br />
+<h2>
+
+Our NYC plumbers are committed to:
+</h2>
+<ol>
+    <li>
+    Always being on time
+    </li>
+    <li>
+    Showing up uniformed and ready to work
+    </li>
+    <li>
+    Carrying the necessary equipment for the task
+    </li>
+    <li>
+    Maintaining a neat workplace
+    </li>
+    <li>
+    Leaving your home in the condition they found it
+    </li>
+    <li>
+    Listening to you and keeping you informed
+    </li>
+</ol>
+<br />
+<br />
+
+
+
+
+
+
+<p>
+
+When you rely on Mario Services, Inc., you never need to worry about plumbing work being an unsightly hassle. We will find the cleanest and safest way to efficiently and thoroughly complete your project.
+</p>
+
+<h2>
+
+24/7 Emergency Repairs
+</h2>
+<br />
+<br />
+<p>
+
+We know that nothing can bring you down like a severe plumbing problem in the wee hours of the morning. When you need emergency service, you want a plumber you can count on to fix the issue right away.
+</p>
+<br />
+<br />
+<p>
+
+At Mario Services, Inc., we have years of experience taking care of customers who found themselves in a heap of trouble. Whatever issue you are facing, we know how to quickly diagnose the problem and have the skills and tools to provide an effective, on-the-spot solution. Don’t wait for someone you can’t trust. Call our team for immediate service!
+</p>
+<br />
+<h5>
+
+Book a Service with Us Today!
+</h5>
+<br />
+<p>
+
+Got a plumbing issue? Not a problem. Our NYC plumbers are standing by, ready to assist you. Regardless of your particular needs, we are willing to do whatever it takes to get the job done right.
+</p>
+        </div>
+    )
+}
+
+export default Services;
